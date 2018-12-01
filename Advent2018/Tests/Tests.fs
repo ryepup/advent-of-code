53e21d98f399ac46d2d6b656ef535864a8bb6c67
Ryan Davis <ryepup@gmail.com>
2018-12-01 02:55:05 -0500

    Day 1: Solve the second puzzle
    Having trouble with the vscode/ionide plugin; it report compiler
    errors that don't exist.
    
    Originally had `frequencies` as a `List`, but `List.contains` is
    O(N) and that was too slow. Using a `Set` worked great.
    
    Some f# observations:
    
    * namespaces vs modules are a little confusing
    * I have no type annotations, but this whole thing is strongly typed;
      the inference is really good. This is great, but also can be confusing
      to try to track down why the inference is making it's decision.
      I got lost for awhile when conveting to `Set` because I still had
      a `List.length` instead of `Set.count`

--- Advent2018/Tests/Tests.fs
+++ Advent2018/Tests/Tests.fs
@@ -4,10 +4,20 @@ open Expecto
 open Advent2018
 open Expecto.Flip
 
+let scanTestCase name deltas expected =
+    testCase name <| fun _ ->
+        // WTF do I need to do so vscode+ionide will recognize `Day1` ?!
+        Day1.scan deltas
+        |> Expect.equal "repeated frequency" expected
+
 [<Tests>]
 let tests =
-  testList "setup" [
+  testList "advent2018" [
     testCase "can reference code in another project" <| fun _ ->
       let sut = Advent2018()
       sut.X |> Expect.equal "X is static" "F#"
+    scanTestCase "simple" [1;-1] 0
+    scanTestCase "still simple" [3;3;4;-2;-4] 10
+    scanTestCase "requires more than one cycle" [-6;3;8;5;-6] 5
+    scanTestCase "requires many cycles" [7;7;-2;-7;-4] 14
   ]
