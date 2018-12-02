7b714e12e8f21f6f95134c8662c9f3667b0b6372
Ryan Davis <ryepup@gmail.com>
2018-12-02 16:19:14 -0500

    Move Day1 code into it's own file

---
+++ Advent2018/Advent2018/Day1.fs
@@ -0,0 +1,25 @@
+namespace Advent2018
+open System.IO
+
+module Day1 =
+    let frequencyChanges =
+        "Advent2018/Advent2018/day1-frequency.txt"
+        |> File.ReadAllLines
+        |> Array.map System.Int32.Parse
+        |> List.ofArray
+
+    let day1 = List.sum frequencyChanges
+
+    let rec scan' deltas frequencies originalDeltas cycle current =
+        match deltas with
+            | [] ->
+                printfn "Cycling back around %i, candidates %i" cycle (Set.count frequencies)
+                scan' originalDeltas frequencies originalDeltas (1+cycle) current
+            | delta :: rest ->
+                let newestFrequency = current + delta
+                match Set.contains newestFrequency frequencies with
+                    | true -> newestFrequency
+                    | false -> scan' rest (Set.add newestFrequency frequencies) originalDeltas cycle newestFrequency
+
+    let scan deltas =
+        scan' deltas (Set.empty.Add(0)) deltas 0 0
