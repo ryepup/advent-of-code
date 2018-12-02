ff1fa50ee6c0219e17a45957fa0ec08b6c3ce482
Ryan Davis <ryepup@gmail.com>
2018-12-02 15:41:03 -0500

    Day 2
    Learning more idioms around pattern matching and composition

--- Advent2018/Advent2018/Advent2018.fs
+++ Advent2018/Advent2018/Advent2018.fs
@@ -1,8 +1,8 @@
 namespace Advent2018
+open System.IO
+open System
 
 module Day1 =
-    open System.IO
-
     let frequencyChanges =
         "Advent2018/Advent2018/day1-frequency.txt"
         |> File.ReadAllLines
@@ -24,3 +24,65 @@ module Day1 =
 
     let scan deltas =
         scan' deltas (Set.empty.Add(0)) deltas 0 0
+
+module Day2 =
+    let ids =
+        "Advent2018/Advent2018/day2.txt"
+        |> File.ReadAllLines
+
+    let hasRepeats n input =
+        input
+        |> Seq.countBy id
+        |> Seq.exists (fun (_, count) -> count = n)
+
+    let hasDouble input =
+        hasRepeats 2 input
+    let hasTriple input =
+        hasRepeats 3 input
+
+    let checksum ids =
+        ids
+        |> Seq.fold (fun acc input ->
+            Seq.zip acc [hasDouble; hasTriple]
+            |> Seq.map
+                (fun (value, pred) -> if pred input then value + 1 else value)
+            ) (Seq.ofList [0; 0])
+        |> Seq.reduce ( * )
+
+    let hasOneCharDifferent a b =
+        Seq.map2
+            (fun charA charB -> if charA = charB then 0 else 1)
+            a b
+        |> Seq.sum
+        |> function
+            | 1 -> true
+            | _ -> false
+
+    let findCommonId (a:string) (b:string) =
+        Seq.zip a b
+        |> Seq.filter (fun (a, b) -> a = b)
+        |> Seq.map (fun (a,_) -> a)
+        |> String.Concat
+
+    let findCode ids =
+        let findMatch id i =
+            ids
+            |> Seq.skip i
+            |> Seq.tryFind (hasOneCharDifferent id)
+
+        ids
+        |> Seq.mapi (fun i id ->
+            match findMatch id i with
+                | None -> None
+                | Some value -> Some (id, value)
+        )
+        |> Seq.choose id // drop the Nones
+        |> Seq.tryHead
+        |> function
+            | None -> failwith "no ids found"
+            | Some (a,b) -> findCommonId a b
+
+
+//findCode ids
+// guess 1, wrong: mphcuasvrnjzzakbgdtqeoylva
+// guess 2, right: mphcuasvrnjzzkbgdtqeoylva
--- Advent2018/Tests/Tests.fs
+++ Advent2018/Tests/Tests.fs
@@ -9,11 +9,35 @@ let scanTestCase name deltas expected =
         Day1.scan deltas
         |> Expect.equal "repeated frequency" expected
 
+let hasRepeatsTestCase input isDouble isTriple =
+    testList (sprintf "testing %s" input) [
+        testCase "isDouble" <| fun _ ->
+            Day2.hasDouble input |> Expect.equal "double?" isDouble
+        testCase "isTriple" <| fun _ ->
+            Day2.hasTriple input |> Expect.equal "triple?" isTriple
+    ]
+
+
 [<Tests>]
 let tests =
-  testList "advent2018" [
-    scanTestCase "simple" [1;-1] 0
-    scanTestCase "still simple" [3;3;4;-2;-4] 10
-    scanTestCase "requires more than one cycle" [-6;3;8;5;-6] 5
-    scanTestCase "requires many cycles" [7;7;-2;-7;-4] 14
-  ]
+    testList "advent" [
+        testList "day1" [
+            scanTestCase "simple" [1;-1] 0
+            scanTestCase "still simple" [3;3;4;-2;-4] 10
+            scanTestCase "requires more than one cycle" [-6;3;8;5;-6] 5
+            scanTestCase "requires many cycles" [7;7;-2;-7;-4] 14
+        ]
+        testList "day2" [
+            hasRepeatsTestCase "abcdef" false false
+            hasRepeatsTestCase "bababc" true true
+            hasRepeatsTestCase "abbcde" true false
+            hasRepeatsTestCase "abcccd" false true
+            hasRepeatsTestCase "aabcdd" true false
+            hasRepeatsTestCase "abcdee" true false
+            hasRepeatsTestCase "ababab" false true
+            testCase "checksum" <| fun _ ->
+                Day2.checksum ["abcdef"; "bababc";"abbcde";"abcccd";"aabcdd";"abcdee";"ababab"]
+                |> Expect.equal "" 12
+        ]
+
+    ]
